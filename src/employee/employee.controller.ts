7657f555c1771221243fee64df0f1e0782ac0834
Souvik Kumar <souvik.cyc@gmail.com>
2024-09-28 23:33:25 +0530

    add: employee controller

---
+++ src/employee/employee.controller.ts
@@ -0,0 +1,32 @@
+import { Controller, Post, Body, Get, Param, Put, Delete } from '@nestjs/common';
+import { EmployeeService } from './employee.service';
+
+@Controller('employee')
+export class EmployeeController {
+  constructor(private readonly employeeService: EmployeeService) {}
+
+  @Post()
+  create(@Body() createEmployeeDto: any) {
+    return this.employeeService.create(createEmployeeDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.employeeService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.employeeService.findOne(id);
+  }
+
+  @Put(':id')
+  update(@Param('id') id: string, @Body() updateEmployeeDto: any) {
+    return this.employeeService.update(id, updateEmployeeDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.employeeService.delete(id);
+  }
+}
