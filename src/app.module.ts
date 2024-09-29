f38e172d3685dae8d0e9233ebdc5a0878f6a04c4
Souvik Kumar <souvik.cyc@gmail.com>
2024-09-29 21:31:15 +0530

    Integrate: Prometheus

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,14 @@ import { AppService } from './app.service';
 import { EmployeeModule } from './employee/employee.module';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { PrometheusModule } from '@willsoto/nestjs-prometheus';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     MongooseModule.forRoot(process.env.MONGODB_URI),
     EmployeeModule,
+    PrometheusModule.register(),
   ],
   controllers: [AppController],
   providers: [AppService],
